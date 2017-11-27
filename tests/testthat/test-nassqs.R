903ef41a609cfe24841edeaf782ab3a93eeea553
Nicholas Potter <potterzot@gmail.com>
2017-11-27 12:09:46 -0800

    remove tests that depend on api key.

--- tests/testthat/test-nassqs.R
+++ tests/testthat/test-nassqs.R
@@ -8,12 +8,12 @@ test_that("nassqs_record_count returns numeric.", {
     commodity_desc = "CORN",
     year = "2012"
   )
-  v = nassqs_record_count(params)
+  #v = nassqs_record_count(params)
 })
 
 test_that("nassqs_param_values returns list of characters.", {
-  v = nassqs_param_values("statisticcat_desc")
-  expect_is(v, "list")
-  expect_is(v[1], "list")
+  #v = nassqs_param_values("statisticcat_desc")
+  #expect_is(v, "list")
+  #expect_is(v[1], "list")
 })
 
